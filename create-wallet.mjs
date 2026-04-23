3ac42850a3bd936c4bd07f4ac5b88f6017657390
Emmanuel Boluwatife Janet <88049758+Tifem@users.noreply.github.com>
2026-04-23 02:01:57 +0100

    first commit

---
+++ create-wallet.mjs
@@ -0,0 +1,20 @@
+import { generateKeyPairSigner, createSolanaRpc, devnet, } from "@solana/kit";
+// Generate a brand new keypair
+const rpc = createSolanaRpc(devnet("https://api.devnet.solana.com"));
+
+const wallet = await generateKeyPairSigner();
+
+console.log(wallet)
+
+// const { value: balance } = await rpc.getBalance(address(“YOUR_ADDRESS_HERE”)).send();
+const { value: balance } = await rpc.getBalance("BqSUYSRox7BuUpNZknuoupXfTR92epr2XaMhu7MpFdfk").send();
+const balanceInSol = Number(balance) / 1_000_000_000;
+
+console.log(`Balance: ${ balanceInSol } SOL`);
+
+console.log("Wallet address:", wallet.address);
+console.log("\n— Go to https://faucet.solana.com/ and airdrop SOL to this address —");
+console.log("— Then run this script again with the same address to check the balance —\n");
+console.log("Your new wallet address:”, wallet.address");
+console.log("\nThis address is your public key.It’s safe to share.");
+console.log("The private key stays in memory.In a real app, you’d save it securely.");
